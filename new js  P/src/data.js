308dffa733cce32c6af5ccb64a6709f46e002c24
GEORGE MICHAEL <112630656+GeorgeMichael2@users.noreply.github.com>
2023-01-14 11:05:44 +0100

    Add files via upload

---
+++ new js  P/src/data.js
@@ -0,0 +1,50 @@
+let shopItemsData = [{
+    id:"hfgh",
+    name: "Black benz",
+    price: "45",
+    desc: "Lorem ipsum dolor sit amet .",
+    img: "image/m1b.jpg"
+},
+{
+    id:"dggh",
+    name: "white toyota",
+    price: "455",
+    desc: "Lorem ipsum dolor sit amet .",
+    img: "image/t1w.jpg"
+},
+{
+    id:"hfavcm",
+    name: "Lexus",
+    price: "35",
+    desc: "Lorem ipsum dolor sit amet .",
+    img: "image/l1b.jpg",
+},
+{
+    id:"hfghsgv",
+    name: "White benz",
+    price: "450",
+    desc: "Lorem ipsum dolor sit amet slv.v.",
+    img: "image/m1w.jpg"
+},
+{
+    id:"p",
+    name: "car1",
+    price: "100",
+    desc: "Lorem ipsum dolor sit amet slv.v.",
+    img: "image/p.jpg"
+},
+{
+    id:"p1",
+    name: "car2",
+    price: "500",
+    desc: "Lorem ipsum dolor sit amet slv.v.",
+    img: "image/pi.jpg"
+},
+{
+    id:"pip",
+    name: "car3",
+    price: "200",
+    desc: "lorem a knx wj jw kdj",
+    img: "image/pip.jpg"
+}
+];
